eb3770061b043ed8a8bc48dceab1fe452dd820d3
gariigolt <132780661+gariigolt@users.noreply.github.com>
2025-03-24 14:39:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hoverArea = document.querySelector('.hover-area');
     const hoverImage = document.querySelector('.hover-image');
+    const eyePath = document.getElementById('eye-path');
 
-    if (hoverArea && hoverImage) {
+    if (hoverArea && hoverImage && eyePath) {
         hoverArea.addEventListener('mousemove', function(e) {
             const rect = hoverArea.getBoundingClientRect();
             const x = e.clientX - rect.left;
             const y = e.clientY - rect.top;
 
-            const eyePath = `path('M ${x - 150},${y} Q ${x - 90},${y - 90} ${x},${y - 90} Q ${x + 90},${y - 90} ${x + 150},${y} Q ${x + 90},${y + 90} ${x},${y + 90} Q ${x - 90},${y + 90} ${x - 150},${y} Z')`;
-
-            hoverImage.style.clipPath = eyePath;
+            const pathData = `M ${x - 150},${y} Q ${x - 90},${y - 90} ${x},${y - 90} Q ${x + 90},${y - 90} ${x + 150},${y} Q ${x + 90},${y + 90} ${x},${y + 90} Q ${x - 90},${y + 90} ${x - 150},${y} Z`;
+            
+            eyePath.setAttribute('d', pathData);
         });
 
         hoverArea.addEventListener('mouseleave', function() {
-            hoverImage.style.clipPath = 'inset(0 0 0 0)';
+            eyePath.setAttribute('d', '');
         });
     }
 });
